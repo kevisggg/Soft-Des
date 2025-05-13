250005bec5cb6eb283fab4c32cff44a9d632ba0e
kevisggg <106260582+kevisggg@users.noreply.github.com>
2025-05-13 14:36:54 +0800

    Update App.java

--- Pacman/src/App.java
+++ Pacman/src/App.java
@@ -1,6 +1,5 @@
 import javax.swing.JFrame;
 
-//lacking features (note): Game Over feature, power pellets, scared ghost, cherry
 public class App {
     public static void main(String[] args) throws Exception {
         int rowCount = 21;
